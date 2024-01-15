78ae84bb888dd93d0fddaf1dfce6b288c550e945
Pravin Patil <106026547+pravin8007@users.noreply.github.com>
2024-01-15 11:27:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-//your JS code here. If required.
- const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
 
         // Function to strip off "a," "an," and "the" from the beginning of the band names
         const stripArticles = (bandName) => {
-            return bandName.replace(/^(A|An the)\s+/i, '');
+            return bandName.replace(/^(a|an)\s+/i, '');
         };
 
         // Sort the bands without considering "a," "an," and "the"
@@ -11,7 +10,7 @@
 
         // Display the sorted bands on the webpage
         const bandList = document.getElementById('bands');
-        sortedBands.forEach(band => {
+        sortedBands.forEach((band, index) => {
             const listItem = document.createElement('li');
             listItem.textContent = stripArticles(band);
             bandList.appendChild(listItem);
